a0709329c05ef8425a1c9788258882639b479d4e
hermanvadym <vadymherman@gmail.com>
2025-05-30 22:28:02 +0200

    D: added player's rotation: left/right

--- main.py
+++ main.py
@@ -21,8 +21,11 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		
+		player.update(dt)
+
 		screen.fill("black")
 		player.draw(screen)
+		
 		pygame.display.flip()
 		
 		# limit the framerate to 60 FPS	
--- player.py
+++ player.py
@@ -1,5 +1,7 @@
+import pygame
 from constants import *
-from circleshape import *
+from circleshape import CircleShape
+from main import *
 
 class Player(CircleShape):
 	def __init__(self, x, y):
@@ -15,5 +17,19 @@ class Player(CircleShape):
 		c = self.position - forward * self.radius + right
 		return [a, b, c]	
 
+
 	def draw(self, screen):
 		pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+
+	def rotate(self, dt):
+		self.rotation += PLAYER_TURN_SPEED * dt
+
+
+	def update(self, dt):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_a]:
+			self.rotate(-dt)
+		if keys[pygame.K_d]:
+			self.rotate(dt)
