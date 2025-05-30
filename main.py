7af2ce607c672f5cf72dc3c11988d0a4d1c66f7a
hermanvadym <vadymherman@gmail.com>
2025-05-30 21:36:16 +0200

    B: added FPS limits and sprites

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ from constants import *
 
 def main():
 	pygame.init
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+	
 	print("Starting Asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	
+	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		pygame.Surface.fill(screen, (0, 0, 0))
+		
+		screen.fill("black")
 		pygame.display.flip()
+		
+		# limit the framerate to 60 FPS	
+		dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
