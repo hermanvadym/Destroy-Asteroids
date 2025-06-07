55dc2b9fe4ff51955587f8ac51bf27799a5fc072
hermanvadym <vadymherman@gmail.com>
2025-06-07 16:10:58 +0200

    H: added destruction for shot and asteroid when collide

--- main.py
+++ main.py
@@ -44,6 +44,10 @@ def main():
 			if asteroid.collides_with(player):
 				print("Game over!")
 				sys.exit()
+			for shot in shots:
+				if shot.collides_with(asteroid):
+					shot.kill()
+					asteroid.kill()
 
 		screen.fill("black")
 
